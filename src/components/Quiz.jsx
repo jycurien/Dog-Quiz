2f65c754238785a91f12e4b411b1b047a9691714
jycurien <curien.jeanyves@gmail.com>
2023-04-10 21:44:15 +0200

    building the list of questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,58 @@
+import { useState, useEffect } from 'react'
+import shuffleArray from '../utils/shuffleArray'
+import Button from './Button'
+
 const Quiz = () => {
-  return <main>TODO</main>
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [breeds, setBreeds] = useState([])
+  const [questions, setQuestions] = useState([])
+
+  const QUIZ_LENGTH = 5
+
+  const buidQuestionsList = async () => {
+    const breedsArray = shuffleArray(breeds).slice(0, QUIZ_LENGTH)
+    setQuestions(breedsArray)
+    console.log(breedsArray)
+  }
+
+  useEffect(() => {
+    const fetchBreeds = async () => {
+      const res = await fetch('https://dog.ceo/api/breeds/list/all')
+      if (res.ok) {
+        const data = await res.json()
+        const tempBreeds = []
+        for (let breed in data.message) {
+          if (data.message[breed].length > 0) {
+            tempBreeds.push(
+              ...data.message[breed].map((subBreed) => `${breed}/${subBreed}`)
+            )
+          } else {
+            tempBreeds.push(breed)
+          }
+        }
+        setBreeds(tempBreeds)
+      } else {
+        setErrorMessage('Something went wrong!')
+      }
+
+      return res
+    }
+    fetchBreeds()
+  }, [])
+
+  if (errorMessage) {
+    return (
+      <main>
+        <div className='error'>{errorMessage}</div>
+      </main>
+    )
+  }
+
+  return (
+    <main>
+      <Button onClick={buidQuestionsList}>Start Quiz</Button>
+    </main>
+  )
 }
 
 export default Quiz
---
+++ src/utils/shuffleArray.js
@@ -0,0 +1,17 @@
+/*
+ * Randomize array element order in-place. Using Durstenfeld shuffle algorithm.
+ * https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+ * @returns {array}
+ */
+const shuffleArray = (array) => {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1))
+    var temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array
+}
+
+export default shuffleArray
