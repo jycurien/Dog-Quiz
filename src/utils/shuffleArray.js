e0d56eed81f6fc6bb87c5297e2a064dc106a87c8
jycurien <curien.jeanyves@gmail.com>
2023-04-10 22:19:15 +0200

    shuffle array

--- src/utils/shuffleArray.js
+++ src/utils/shuffleArray.js
@@ -5,9 +5,9 @@
  * @returns {array}
  */
 const shuffleArray = (array) => {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1))
-    var temp = array[i]
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = array[i]
     array[i] = array[j]
     array[j] = temp
   }
